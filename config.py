e84e294e605ff74bf19f36135c0eb7023001f52a
Siyu Chen <siyuchen@umich.edu>
2015-12-01 19:06:33 -0500

    add unlearnt recipe

--- config.py
+++ config.py
@@ -5,16 +5,10 @@ import os
 class BaseConfig(object):
     DEBUG = False
     # shortened for readability
-    SECRET_KEY = '\xbf\xb0\x11\xb1\xcd\xf9\xba\x8bp\x0c...'
+    SECRET_KEY = "\x03\x03\xfc\x02\xc2f\xf3\x84'\xa8V\x9e\x97\xc3\xe1hB\xd7\xb3\x1b\xde\xd3\x13\xa5"
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     print SQLALCHEMY_DATABASE_URI
 
-
-class TestConfig(BaseConfig):
-    DEBUG = True
-    TESTING = True
-    WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 
 class DevelopmentConfig(BaseConfig):
