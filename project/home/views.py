db1662c6df97ff0b23317b33036e81b12d15a837
xiaofji <xiaofji@umich.edu>
2015-12-01 21:24:26 -0500

    Merge branch 'master' into cookingpage
    # Conflicts:
    #	config.pyc
    #	project/__init__.pyc
    #	project/home/__init__.pyc
    #	project/home/forms.pyc
    #	project/home/views.pyc
    #	project/models.pyc
    #	project/users/__init__.pyc
    #	project/users/forms.pyc
    #	project/users/views.pyc
    #	templates/base.html

--- project/home/views.py
+++ project/home/views.py
@@ -24,9 +24,38 @@ home_blueprint = Blueprint(
 #### routes ####
 ################
 @home_blueprint.route("/", methods=['GET', 'POST'])
+@home_blueprint.route("/index", methods=['GET', 'POST'])
 # @login_required
 def default():
     form = LoginForm(request.form)
+    error = None
+    if request.method == 'POST':
+        if request.form['submit'] == 'login':
+            if form.validate_on_submit():
+                user = User.query.filter_by(username=request.form['username']).first()
+                print user
+                if user is not None and bcrypt.check_password_hash(
+                    user.password, request.form['password']
+                ):
+                    login_user(user)
+                else:
+                    error = 'Invalid username or password.'
+        elif request.form['submit'] == 'signup':
+            return redirect(url_for('users.register'))
+        elif request.form['submit'] == 'logout':
+            logout_user()
+            return render_template("index.html", name = "index", title = "WELCOME", form=form, user=current_user)
+
+
+    return render_template("index.html", name = "index", title = "WELCOME", error = error, form=form, user=current_user)
+
+
+
+@home_blueprint.route("/yuecai/<LearningRecipeName>", methods=['GET', 'POST'])
+def yuecai(LearningRecipeName):
+    error = None
+    form = LoginForm(request.form)
+    formR = AddRecipeForm(request.form)
     if request.method == 'POST':
         if request.form['submit'] == 'login':
             if form.validate_on_submit():
@@ -44,30 +73,9 @@ def default():
         elif request.form['submit'] == 'logout':
             logout_user()
             return render_template("index.html", name = "index", title = "WELCOME", form=form, user=current_user)
-
-
-    return render_template("index.html", name = "index", title = "WELCOME", form=form, user=current_user)
-
-@home_blueprint.route("/index")
-def index():
-    return render_template("index.html", name = "index", title = "WELCOME")
-
-@home_blueprint.route("/yuecai")
-def yuecai():
-    return render_template("yuecai.html", name = "yuecai", title = "YUECAI")
-# use decorators to link the function to a url
-@home_blueprint.route('/personalpage', methods=['GET', 'POST'])
-@login_required
-def personalpage():
-    # return "Hello, World!"  # return a string
-    # form = LoginForm(request.form)
-    # user = User.query.filter_by(username=request.form['username']).first()
-    form = AddRecipeForm(request.form)
-
-    if request.method == 'POST':
-        if form.validate_on_submit():
+        else:
             user=User.query.filter_by(username=current_user.username).first()
-            recipe = Recipe.query.filter_by(recipename=request.form['recipename']).first()
+            recipe = Recipe.query.filter_by(recipename=request.form['submit']).first()
             if recipe is not None:
                 recipes = Recipe.query.filter(Recipe.users.any(username=current_user.username)).all()
 
@@ -77,9 +85,37 @@ def personalpage():
                     # commit the changes
                     db.session.commit()
 
+    return render_template("yuecai.html", name = "yuecai", title = "YUECAI", form=form, user=current_user, error=error, LearningRecipeName = LearningRecipeName)
+# use decorators to link the function to a url
+@home_blueprint.route('/personalpage', methods=['GET', 'POST'])
+@login_required
+def personalpage():
+
+    error = None
+    recipes = None
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        if request.form['submit'] == 'login':
+            if form.validate_on_submit():
+                user = User.query.filter_by(username=request.form['username']).first()
+                print user
+                if user is not None and bcrypt.check_password_hash(
+                    user.password, request.form['password']
+                ):
+                    login_user(user)
+
+                else:
+                    error = 'Invalid username or password.'
+        elif request.form['submit'] == 'signup':
+            return redirect(url_for('users.register'))
+        elif request.form['submit'] == 'logout':
+            logout_user()
+            return redirect(url_for('home.personalpage', recipes=recipes, form=form, user=current_user))  # render a template
+
     recipes = Recipe.query.filter(Recipe.users.any(username=current_user.username)).all()
+    recipesNotLearnt = Recipe.query.filter(~Recipe.users.any(username=current_user.username)).all()
     # recipes = db.session.query(Recipe).all()
-    return render_template('personalpage.html', recipes=recipes, form=form, user=current_user)  # render a template
+    return render_template('personalpage.html', recipes=recipes, recipesNotLearnt = recipesNotLearnt, form=form, user=current_user)  # render a template
 
     # posts = db.session.query(BlogPost).all()
     # return render_template('index.html', posts=posts)  # render a template
